3269b1e99590cd06d9ba39b7db43caa79d37fbe7
Miko Meller <miko.meller@cloudreach.com>
2022-11-07 14:04:08 +0100

    full collection of app-infra as of 20221107

---
+++ main.tf
@@ -0,0 +1,38 @@
+data "aws_vpc" "main" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+data "aws_subnet" "subnet_g_ls_e" {
+  filter {
+    name   = "tag:Name"
+    values = [var.private_subnet_name]
+  }
+}
+
+data "aws_ami" "aws_ubuntu_image" {
+  owners      = [var.aws_ami_owner]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = [var.aws_ami_name]
+  }
+}
+
+resource "aws_ebs_volume" "ebs_logstash" {
+  availability_zone = "eu-central-1a"
+  size              = 50
+
+  tags = {
+    Name = "EBS"
+  }
+}
+
+data "aws_subnet" "subnet_kibana" {
+  filter {
+    name   = "tag:Name"
+    values = [var.public_subnet_name]
+  }
+}
